4f7cb23d459dcbcb382f5078c48b7e3f7b688644
Dominick Haggerty (Class of 2025) <250700dh@salesianum.org>
2023-10-18 13:56:33 -0400

    madlib

--- madlib.py
+++ madlib.py
@@ -1,10 +1,10 @@
-passage = '''I remember sittin' at that house 
-Thinkin' no way them boys wouldn't win 
-Between them, big three pitchers 
-Andrew and chipper 
-It was gonna be hard to keep up with the Jones 
-But as fate would have it 
-That Atlanta magic 
+passage = '''I remember sittin' at that house
+Thinkin' no way them boys wouldn't win
+Between them, big three pitchers
+Andrew and chipper
+It was gonna be hard to keep up with the Jones
+But as fate would have it
+That Atlanta magic
 Got put out by them Padres'''
 
 newPara = ""
@@ -25,7 +25,7 @@ newText1 = input("enter a location: ")
 newPara += newText1
 
 start = end + 5
-end = start + 2
+end = start + 1
 
 newPara += passage[start:end]
 newText1 = input('''enter an "i n g" verb: ''')
@@ -45,29 +45,54 @@ newPara += passage[start:end]
 newText1 = input("enter a verb: ")
 newPara += newText1
 
-start = end + 4
-end = start + 17
+start = end + 3
+end = start + 15
 
 newPara += passage[start:end]
-newText1 = input("enter an adjective: ")
+newText1 = input("enter a adjective: ")
+newPara += newText1
+
+start = end + 3
+end = start + 1
+
+newPara += passage[start:end]
+newText1 = input("enter a number greater than 1: ")
 newPara += newText1
 
 start = end + 5
 end = start + 1
 
 newPara += passage[start:end]
-newText1 = input("enter a number: ")
+newText1 = input("enter a person, place, or thing: ")
 newPara += newText1
 
-start = end + 1
-end = start + 8
+start = end + 8
+end = start + 1
 
 newPara += passage[start:end]
-newText1 = input("enter person, place, or thing: ")
+newText1 = input("enter a person: ")
 newPara += newText1
 
-start = end + 1
-end = start + 1
+start = end + 6
+end = start + 5
+
+newPara += passage[start:end]
+newText1 = input("enter a person: ")
+newPara += newText1
+
+start = end + 7
+end = start + 17
+
+newPara += passage[start:end]
+newText1 = input("enter an adjective: ")
+newPara += newText1
+
+start = end + 4
+end = start + 17
+
+newPara += passage[start:end]
+newText1 = input("enter a person: ")
+newPara += newText1
 
 print(newPara)                                                                
 
