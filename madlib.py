ee9e4779996829d9880a6f65ab9841311e9c9651
Dominick Haggerty (Class of 2025) <250700dh@salesianum.org>
2023-10-20 10:55:33 -0400

    madlib

--- madlib.py
+++ madlib.py
@@ -5,7 +5,7 @@ Andrew and chipper
 It was gonna be hard to keep up with the Jones
 But as fate would have it
 That Atlanta magic
-Got put out by them Padres'''
+Got putout by them Padres'''
 
 newPara = ""
 
@@ -80,18 +80,74 @@ newPara += passage[start:end]
 newText1 = input("enter a person: ")
 newPara += newText1
 
-start = end + 7
-end = start + 17
+start = end + 8
+end = start
+
+newPara += passage[start:end]
+newText1 = input("enter a pronoun : ")
+newPara += newText1
+
+start = end + 2
+end = start + 14
 
 newPara += passage[start:end]
 newText1 = input("enter an adjective: ")
 newPara += newText1
 
 start = end + 4
-end = start + 17
+end = start + 21
 
 newPara += passage[start:end]
-newText1 = input("enter a person: ")
+newText1 = input("enter someone: ")
+newPara += newText1
+
+start = end + 5
+end = start + 8
+
+newPara += passage[start:end]
+newText1 = input("enter an noun: ")
+newPara += newText1
+
+start = end + 4
+end = start + 12
+
+newPara += passage[start:end]
+newText1 = input("enter an pronoun: ")
+newPara += newText1
+
+start = end + 2
+end = start + 6
+
+newPara += passage[start:end]
+newText1 = input("enter an city: ")
+newPara += newText1
+
+start = end +7
+end = start + 1
+
+newPara += passage[start:end]
+newText1 = input("enter an noun: ")
+newPara += newText1
+
+start = end + 5
+end = start + 1
+
+newPara += passage[start:end]
+newText1 = input("enter an past tense verb: ")
+newPara += newText1
+
+start = end + 3
+end = start + 1
+
+newPara += passage[start:end]
+newText1 = input("enter an group verb: ")
+newPara += newText1
+
+start = end + 6
+end = start + 9
+
+newPara += passage[start:end]
+newText1 = input("enter an noun: ")
 newPara += newText1
 
 print(newPara)                                                                
