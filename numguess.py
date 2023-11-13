83dad5d440707f9b7c65fda847ac0a3c785e5f34
Dominick Haggerty (Class of 2025) <250700dh@salesianum.org>
2023-11-13 10:14:39 -0500

    numguess.py

--- numguess.py
+++ numguess.py
@@ -1,9 +1,17 @@
 import random
 
+while True:
+
+    number = random.randint(1, 100)
+    print(number)
+
+    player = int(input("guess a number:    "))
 
 
-number = random.randint(1, 100)
-print(number)
+    if(player == number):
+        print("That is the number")
+
+    elif(player > number):
 
 
 
